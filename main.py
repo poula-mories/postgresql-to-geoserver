1ec473986665cc3d3be4980c0dc40f642ffe8903
Poula Mories <poulamories5@gmail.com>
2024-03-12 16:52:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,178 +2,250 @@ import json
 import psycopg2
 import requests
 import paramiko
+from psycopg2 import OperationalError
 
 # Function to read parameters from JSON configuration file
 def read_config(filename):
-    with open(filename, 'r') as file:
-        config = json.load(file)
-    return config
+    try:
+        with open(filename, 'r') as file:
+            config = json.load(file)
+        return config
+    except FileNotFoundError:
+        print(f"Error: File '{filename}' not found.")
+        return None
+    except json.JSONDecodeError as e:
+        print(f"Error: Invalid JSON format in '{filename}': {e}")
+        return None
+    except Exception as e:
+        print(f"An unexpected error occurred while reading '{filename}': {e}")
+        return None
 
 # Function to connect to the PostgreSQL database
 def connect_to_postgres(host, port, user, password, dbname):
-    conn = psycopg2.connect(
-        host=host,
-        user=user,
-        password=password,
-        dbname=dbname,
-        port=port
-    )
-    return conn
+        try:
+            conn = psycopg2.connect(
+                host=host,
+                user=user,
+                password=password,
+                dbname=dbname,
+                port=port
+            )
+            return conn
+        except OperationalError as e:
+            print(f"There is error while trying to connect the Postgresql database: {e}")
+            return None
 
 
 # Function to connect to the PostgreSQL database via SSH tunnel
 def connect_to_postgres_by_ssh(host, port, user, password, dbname, enable_ssh, ssh_host, ssh_port, ssh_user, ssh_password=None, ssh_pem_key=None):
-    # Establish SSH connection
-    ssh_client = paramiko.SSHClient()
-    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    try:
+        # Establish SSH connection
+        ssh_client = paramiko.SSHClient()
+        ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        print('Trying to connect SSH')
+        # Use password authentication if ssh_password is provided
+        if ssh_password:
+            ssh_client.connect(ssh_host, port=ssh_port, username=ssh_user, password=ssh_password)
+        elif ssh_pem_key:
+            ssh_key = paramiko.RSAKey.from_private_key_file(ssh_pem_key)
+            ssh_client.connect(ssh_host, port=ssh_port, username=ssh_user, pkey=ssh_key)
+        else:
+            raise ValueError("Either ssh_password or ssh_pem_key must be provided.")
 
-    # Use password authentication if ssh_password is provided
-    if ssh_password:
-        ssh_client.connect(ssh_host, port=ssh_port, username=ssh_user, password=ssh_password)
-    elif ssh_pem_key:
-        ssh_key = paramiko.RSAKey.from_private_key_file(ssh_pem_key)
-        ssh_client.connect(ssh_host, port=ssh_port, username=ssh_user, pkey=ssh_key)
-    else:
-        raise ValueError("Either ssh_password or ssh_pem_key must be provided.")
+        print('SSH tunnel connected successfully')
 
-    # Forward local port to remote PostgreSQL server
-    local_port = 5433  # Choose any available local port
-    ssh_transport = ssh_client.get_transport()
-    ssh_transport.request_port_forward('', local_port)
-    print('SSH tunnel connected successfully')
-    # Connect to PostgreSQL via forwarded port
-    conn = psycopg2.connect(
-        host=host,
-        user=user,
-        password=password,
-        dbname=dbname,
-        port=port)
-    return conn
+        try:
+            # Forward local port to remote PostgreSQL server
+            local_port = 5433  # Choose any available local port
+            ssh_transport = ssh_client.get_transport()
+            ssh_transport.request_port_forward('', local_port)
+
+            try:
+                # Connect to PostgreSQL via forwarded port
+                conn = psycopg2.connect(
+                    host=host,
+                    user=user,
+                    password=password,
+                    dbname=dbname,
+                    port=port)
+                return conn
+            except psycopg2.OperationalError as e:
+                print(f"Error connecting to PostgreSQL: {e}")
+                return None
+        except Exception as e:
+            print(f"Error setting up port forwarding: {e}")
+            return None
+    except paramiko.AuthenticationException as e:
+        print(f"Authentication error: {e}")
+        return None
+    except paramiko.SSHException as e:
+        print(f"SSH connection error: {e}")
+        return None
+    except ValueError as e:
+        print(f"ValueError: {e}")
+        return None
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        return None
 
 # Function to get a list of tables/views from the specified schema
 def get_layers_name(conn, schema):
     layers_name = []
     cursor = conn.cursor()
-    cursor.execute(f"SELECT table_name FROM information_schema.columns WHERE column_name ='geom' and table_schema ='{schema}'")
+    cursor.execute(f"select f_table_name from geometry_columns where f_table_schema ='{schema}'")
     layers_name.extend(cursor.fetchall())
     cursor.close()
     return layers_name
 
 # Function to check if layer exists in GeoServer
-def layer_exists(host, port, user, password, workspace, datastore, layer_name):
-    url = f"http://{host}:{port}/geoserver/rest/workspaces/{workspace}/datastores/{datastore}/featuretypes/{layer_name}"
-    response = requests.get(url, auth=(user, password))
-    return response.status_code == 200
+def layer_exists(url, user, password, workspace, datastore, layer_name):
+    try:
+        req_url = f"{url}/geoserver/rest/workspaces/{workspace}/datastores/{datastore}/featuretypes/{layer_name}"
+        response = requests.get(req_url, auth=(user, password))
+        response.raise_for_status()  # Raise an exception for 4xx or 5xx status codes
+        return response.status_code == 200
+    except requests.RequestException as e:
+        print(f"Error checking layer existence: {e}")
+        return False
 
 # Function to create layer in GeoServer
-def create_layer(host, port, user, password, workspace, datastore, layer_name):
-    url = f"http://{host}:{port}/geoserver/rest/workspaces/{workspace}/datastores/{datastore}/featuretypes"
-    headers = {'Content-Type': 'application/json'}
-    auth = (user, password)
-    data = {
-        "featureType": {
-            "name": layer_name,
-            "nativeName": layer_name,
-            "title": layer_name,
-            "enabled": True
-        }
-    }
-    response = requests.post(url, json=data, headers=headers, auth=auth)
-    return response.status_code == 201
-
-# Function to check if workspace exists in GeoServer
-def workspace_exists(host, port, user, password, workspace):
-    url = f"http://{host}:{port}/geoserver/rest/workspaces/{workspace}"
-    response = requests.get(url, auth=(user, password))
-    return response.status_code == 200
-
-# Function to create workspace in GeoServer
-def create_workspace(host, port, user, password, workspace):
-    url = f"http://{host}:{port}/geoserver/rest/workspaces"
-    headers = {'Content-Type': 'application/xml'}
-    data = f"<workspace><name>{workspace}</name></workspace>"
-    response = requests.post(url, data=data, headers=headers, auth=(user, password))
-    return response.status_code == 201
-
-# Function to check if store exists in GeoServer
-def store_exists(host, port, user, password, workspace, store_name):
-    url = f"http://{host}:{port}/geoserver/rest/workspaces/{workspace}/datastores/{store_name}"
-    response = requests.get(url, auth=(user, password))
-    return response.status_code == 200
-
-def create_store(host, port, user, password, workspace, store_name, dbhost, dbname, dbuser, dbpass, dbport):
-    url = f"http://{host}:{port}/geoserver/rest/workspaces/{workspace}/datastores"
-    headers = {'Content-Type': 'application/json'}
-
-    # Create dictionary representing the data
-    data = {
-                "dataStore": {
-                "name": store_name,
-                "type": "PostGIS",
-                "enabled": True,
-                "connectionParameters": {
-                    "host": dbhost,
-                    "port": dbport,
-                    "database": dbname,
-                    "user": dbuser,
-                    "passwd": dbpass,
-                    "dbtype": "postgis",
-                    "schema": store_name,
-                    "max connections": 10,
-                    "min connections": 1,
-                    "Evictor run periodicity": 300,
-                    "Loose bbox": True,
-                    "SSL mode": "DISABLE",
-                    "Estimated extends": True,
-                    "fetch size": 1000,
-                    "preparedStatements": False,
-                    "Batch insert size": 1,
-                    "encode functions": True,
-                    "Max open prepared statements": 50,
-                    "Expose primary keys": True,
-                    "validate connections": True,
-                    "Support on the fly geometry simplification": True,
-                    "Connection timeout": 20,
-                    "Method used to simplify geometries": "FAST",
-                    "Evictor tests per run": 3,
-                    "Test while idle": True,
-                    "Max connection idle time": 300
-                }
-            }
-    }
-
-    # Convert dictionary to JSON string
-    json_data = json.dumps(data)
-
-    # Send POST request with JSON data
-    response = requests.post(url, data=json_data, headers=headers, auth=(user, password))
-    
-    if response.status_code != 201:
-        print(response.content)
-    else:
-        return response.status_code == 201
-
-# Function to publish layers to GeoServer
-def publish_to_geoserver(host, port, user, password, workspace, datastore, table_name):
-    if not layer_exists(host, port, user, password, workspace, datastore, table_name):
-        url = f"http://{host}:{port}/geoserver/rest/workspaces/{workspace}/datastores/{datastore}/featuretypes"
+def create_layer(url, user, password, workspace, datastore, layer_name):
+    try:
+        req_url = f"{url}/geoserver/rest/workspaces/{workspace}/datastores/{datastore}/featuretypes"
         headers = {'Content-Type': 'application/json'}
         auth = (user, password)
         data = {
             "featureType": {
-                "name": table_name,
-                "nativeName": table_name,
-                "title": table_name,
+                "name": layer_name,
+                "nativeName": layer_name,
+                "title": layer_name,
                 "enabled": True
             }
         }
-        response = requests.post(url, json=data, headers=headers, auth=auth)
-        if response.status_code == 201:
-            print(f"Layer '{table_name}' published successfully to GeoServer.")
+        response = requests.post(req_url, json=data, headers=headers, auth=auth)
+        response.raise_for_status()
+        return response.status_code == 201
+    except requests.RequestException as e:
+        print(f"Error creating layer: {e}")
+        return False
+
+# Function to check if workspace exists in GeoServer
+def workspace_exists(url, user, password, workspace):
+    try:
+        req_url = f"{url}/geoserver/rest/workspaces/{workspace}"
+        response = requests.get(req_url, auth=(user, password))
+        response.raise_for_status()
+        return response.status_code == 200
+    except requests.RequestException as e:
+        print(f"Error checking workspace existence: {e}")
+        return False
+
+# Function to create workspace in GeoServer
+def create_workspace(url, user, password, workspace):
+    try:
+        req_url = f"{url}/geoserver/rest/workspaces"
+        headers = {'Content-Type': 'application/xml'}
+        data = f"<workspace><name>{workspace}</name></workspace>"
+        response = requests.post(req_url, data=data, headers=headers, auth=(user, password))
+        response.raise_for_status()
+        return response.status_code == 201
+    except requests.RequestException as e:
+        print(f"Error creating workspace: {e}")
+        return False
+    
+# Function to check if store exists in GeoServer
+def store_exists(url, user, password, workspace, store_name):
+    try:
+        req_url = f"{url}/geoserver/rest/workspaces/{workspace}/datastores/{store_name}"
+        response = requests.get(req_url, auth=(user, password))
+        response.raise_for_status()
+        return response.status_code == 200
+    except requests.RequestException as e:
+        print(f"Error checking store existence: {e}")
+        return False
+
+def create_store(url, user, password, workspace, store_name, dbhost, dbname, dbuser, dbpass, dbport):
+    try:
+        req_url = f"{url}/geoserver/rest/workspaces/{workspace}/datastores"
+        headers = {'Content-Type': 'application/json'}
+
+        # Create dictionary representing the data
+        data = {
+                    "dataStore": {
+                    "name": store_name,
+                    "type": "PostGIS",
+                    "enabled": True,
+                    "connectionParameters": {
+                        "host": dbhost,
+                        "port": dbport,
+                        "database": dbname,
+                        "user": dbuser,
+                        "passwd": dbpass,
+                        "dbtype": "postgis",
+                        "schema": store_name,
+                        "max connections": 10,
+                        "min connections": 1,
+                        "Evictor run periodicity": 300,
+                        "Loose bbox": True,
+                        "SSL mode": "DISABLE",
+                        "Estimated extends": True,
+                        "fetch size": 1000,
+                        "preparedStatements": False,
+                        "Batch insert size": 1,
+                        "encode functions": True,
+                        "Max open prepared statements": 50,
+                        "Expose primary keys": True,
+                        "validate connections": True,
+                        "Support on the fly geometry simplification": True,
+                        "Connection timeout": 20,
+                        "Method used to simplify geometries": "FAST",
+                        "Evictor tests per run": 3,
+                        "Test while idle": True,
+                        "Max connection idle time": 300
+                    }
+                }
+        }
+
+        # Convert dictionary to JSON string
+        json_data = json.dumps(data)
+
+        # Send POST request with JSON data
+        response = requests.post(req_url, data=json_data, headers=headers, auth=(user, password))
+    
+        if response.status_code != 201:
+            print(response.content)
+            return False
         else:
-            print(f"Failed to publish layer '{table_name}' to GeoServer. Error: {response.text}")
-    else:
-        print(f"Layer '{table_name}' already exists in GeoServer.")
+            return True
+    except requests.RequestException as e:
+        print(f"Error creating store: {e}")
+        return False
+
+# Function to publish layers to GeoServer
+def publish_to_geoserver(url, user, password, workspace, datastore, table_name):
+    try:
+        if not layer_exists(url, user, password, workspace, datastore, table_name):
+            req_url = f"{url}/geoserver/rest/workspaces/{workspace}/datastores/{datastore}/featuretypes"
+            headers = {'Content-Type': 'application/json'}
+            auth = (user, password)
+            data = {
+                "featureType": {
+                    "name": table_name,
+                    "nativeName": table_name,
+                    "title": table_name,
+                    "enabled": True
+                }
+            }
+            response = requests.post(req_url, json=data, headers=headers, auth=auth)
+            response.raise_for_status()
+            if response.status_code == 201:
+                print(f"Layer '{table_name}' published successfully to GeoServer.")
+            else:
+                print(f"Failed to publish layer '{table_name}' to GeoServer. Error: {response.text}")
+        else:
+            print(f"Layer '{table_name}' already exists in GeoServer.")
+    except requests.RequestException as e:
+        print(f"Error publishing to GeoServer: {e}")
 
 # Main function
 def main():
@@ -208,6 +280,9 @@ def main():
     # Get tables/views from specified schema
     for schema in schemas:
         tables = get_layers_name(conn, schema)
+        if (len(tables)<1):
+            print(f"There is no spatial layers inside schema '{schema}'")
+            continue
     
         # Publish each table/view to GeoServer
         for table in tables:
